cd3ceae5a52855d1550d2b503c0c2995d7f6a3c2
citypayorg <citypay.org@gmail.com>
2021-11-08 17:50:06 +0900

    make address

--- routes/index.js
+++ routes/index.js
@@ -16,25 +16,25 @@ const { createUserSchema, updateUserSchema, validateLogin } = require('../app/mi
 const Web3 = require("web3");
 const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.0.185:21004"));
 
-router.get('/', function(req, res, next) {
+// npm i sync-mysql
+var db_config = require(__dirname + '/database.js');// 2020-09-13
+var sync_mysql = require('sync-mysql'); //2020-01-28
+let sync_connection = new sync_mysql(db_config.constr());
 
+router.get('/', function(req, res, next) {
   if (req.cookies.user_idx == "" || req.cookies.user_idx === undefined) {
     res.sendFile(STATIC_PATH + '/ulogin.html')
     return;
   }
   else {
     /////////////////////////
-    // npm i sync-mysql
-    var db_config = require(__dirname + '/database.js');// 2020-09-13
-    var sync_mysql = require('sync-mysql'); //2020-01-28
-    let sync_connection = new sync_mysql(db_config.constr());
-
     let user_email = req.cookies.user_email;
     let result = sync_connection.query("SELECT id, c4ei_addr, c4ei_balance FROM user a WHERE a.email='" + user_email + "'");
     let user_id = result[0].id;
     let c4ei_addr = result[0].c4ei_addr;
     let c4ei_balance = result[0].c4ei_balance;
-    if (c4ei_addr!="" && user_id > 0){
+    console.log("c4ei_addr :"+c4ei_addr);
+    if ((c4ei_addr!="" &&c4ei_addr!=null) && user_id > 0){
       var wallet_balance = web3.eth.getBalance(c4ei_addr, function(error, result) {
         // console.log("wallet_balance : "+ web3.utils.fromWei(result, "ether")); //0x21725F3b26F74C8E451d851e040e717Fbcf19E5b
         wallet_balance = web3.utils.fromWei(result, "ether");
@@ -49,7 +49,7 @@ router.get('/', function(req, res, next) {
     const msg = "https://easy.c4ei.net/rcv?rcv_email="+user_email+"&rcv_adr="+c4ei_addr+"&rcv_amt=0&tt="+Date.now();
     console.log("msg :"+msg);
     QRCode.toDataURL(msg,function(err, url){
-      res.render('index', { title: 'easy', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, dataUrl : url});
+      res.render('index', { title: 'easypay', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, dataUrl : url});
     });
   }
 });
@@ -62,17 +62,13 @@ router.get('/rcv', function(req, res, next) {
   }
   else {
     /////////////////////////
-    // npm i sync-mysql
-    var db_config = require(__dirname + '/database.js');// 2020-09-13
-    var sync_mysql = require('sync-mysql'); //2020-01-28
-    let sync_connection = new sync_mysql(db_config.constr());
-
     let user_email = req.cookies.user_email;
     let result = sync_connection.query("SELECT id, c4ei_addr, c4ei_balance FROM user a WHERE a.email='" + user_email + "'");
     let user_id = result[0].id;
     let c4ei_addr = result[0].c4ei_addr;
     let c4ei_balance = result[0].c4ei_balance;
-    if (c4ei_addr!="" && user_id > 0){
+    // console.log("c4ei_addr :"+c4ei_addr);
+    if ((c4ei_addr!="" &&c4ei_addr!=null) && user_id > 0){
       var wallet_balance = web3.eth.getBalance(c4ei_addr, function(error, result) {
         // console.log("wallet_balance : "+ web3.utils.fromWei(result, "ether")); //0x21725F3b26F74C8E451d851e040e717Fbcf19E5b
         wallet_balance = web3.utils.fromWei(result, "ether");
@@ -88,9 +84,41 @@ router.get('/rcv', function(req, res, next) {
     var rcv_adr = req.query.rcv_adr;
     var rcv_amt = req.query.rcv_amt;
     console.log("rcv_email :"+rcv_email + '/ rcv_adr : ' + rcv_adr);
-    res.render('rcv', { title: 'easyRcv', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, 
+    res.render('rcv', { title: 'easypay Send', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, 
       rcv_email :rcv_email,rcv_adr:rcv_adr,rcv_amt:rcv_amt});
   }
+});
+
+router.get('/address_generator', function(req, res, next) {
+  if (req.cookies.user_idx == "" || req.cookies.user_idx === undefined) {
+    res.sendFile(STATIC_PATH + '/ulogin.html')
+    return;
+  }
+  else {
+    /////////////////////////
+    console.log("address_generator ");
+    let user_email = req.cookies.user_email;
+    let result1 = sync_connection.query("SELECT id, c4ei_addr, c4ei_balance FROM user a WHERE a.email='" + user_email + "'");
+    let user_id = result1[0].id;
+    let c4ei_addr = result1[0].c4ei_addr;
+    // let c4ei_balance = result1[0].c4ei_balance;
+    if ((c4ei_addr=="" ||c4ei_addr==null) && user_id > 0){
+      let result2 = sync_connection.query("SELECT idx,address,cur_bal FROM address WHERE useYN='Y' and mappingYN='N' AND cur_bal=0 AND user_id=0 LIMIT 1");
+      let new_address = result2[0].address;
+      console.log("new_address :"+ new_address);
+      let result3 = sync_connection.query("UPDATE address SET email='"+user_email+"',user_id='" + user_id + "',mappingYN='Y',last_reg=now() WHERE  address ='"+new_address+"'");
+      let result4 = sync_connection.query("UPDATE user SET c4ei_addr='"+new_address+"',last_reg=now() WHERE id='" + user_id + "'");
+      console.log("result4 end");
+    }
+    res.redirect('/');
+    /////////////////////////
+  }
+});
+
+router.get('/logout', function(req, res, next) {
+  res.cookie('user_idx', ''); // 2021-11-08
+  res.cookie('user_email', ''); // 2021-11-08
+  res.redirect('/');
 });
 
 router.get('/', auth(), awaitHandlerFactory(userController.getAllUsers)); // localhost:3000/api/v1/users
