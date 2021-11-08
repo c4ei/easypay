9160489b6d2b910e29faba33a91809f83ca2c36c
citypayorg <citypay.org@gmail.com>
2021-11-08 14:45:31 +0900

    rcv page add

--- routes/index.js
+++ routes/index.js
@@ -16,12 +16,6 @@ const { createUserSchema, updateUserSchema, validateLogin } = require('../app/mi
 const Web3 = require("web3");
 const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.0.185:21004"));
 
-/* GET home page. */
-// router.get('/', async function(req, res, next) {
-//   const msg = "https://easypay.c4ei.net/sAd=assdaasdas&rAd=asadsad&amt=0.0001&tt="+Date.now();
-//   const url = await QRCode.toDataURL(msg);
-//   res.render('index', { title: 'Express', dataUrl : url});
-// });
 router.get('/', function(req, res, next) {
 
   if (req.cookies.user_idx == "" || req.cookies.user_idx === undefined) {
@@ -29,6 +23,7 @@ router.get('/', function(req, res, next) {
     return;
   }
   else {
+    /////////////////////////
     // npm i sync-mysql
     var db_config = require(__dirname + '/database.js');// 2020-09-13
     var sync_mysql = require('sync-mysql'); //2020-01-28
@@ -39,28 +34,64 @@ router.get('/', function(req, res, next) {
     let user_id = result[0].id;
     let c4ei_addr = result[0].c4ei_addr;
     let c4ei_balance = result[0].c4ei_balance;
-    if (c4ei_addr!=""){
-      const wallet_balance = web3.eth.getBalance(c4ei_addr, function(error, result) {
-        console.log("wallet_balance : "+result);
+    if (c4ei_addr!="" && user_id > 0){
+      var wallet_balance = web3.eth.getBalance(c4ei_addr, function(error, result) {
+        // console.log("wallet_balance : "+ web3.utils.fromWei(result, "ether")); //0x21725F3b26F74C8E451d851e040e717Fbcf19E5b
+        wallet_balance = web3.utils.fromWei(result, "ether");
+        if (wallet_balance != c4ei_balance){
+          let result = sync_connection.query("update user set c4ei_balance='"+wallet_balance+"' WHERE id='" + user_id + "'");
+          console.log("wallet_balance :"+wallet_balance);
+          c4ei_balance = wallet_balance;
+        }
       });
     }
-
-    const msg = "https://easy.c4ei.net/remail="+user_email+"&rAd="+c4ei_addr+"&amt=0&tt="+Date.now();
-    // const url = QRCode.toDataURL(msg);
+    /////////////////////////
+    const msg = "https://easy.c4ei.net/rcv?rcv_email="+user_email+"&rcv_adr="+c4ei_addr+"&rcv_amt=0&tt="+Date.now();
+    console.log("msg :"+msg);
     QRCode.toDataURL(msg,function(err, url){
       res.render('index', { title: 'easy', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, dataUrl : url});
     });
-    // // res.render('pages/login', get_user_info_json(user_id));
-    // try{
-    //   console.log('######### index.js 119 get_user_info_json ######### '+req.cookies.user_idx+' #########');
-    //   res.render('pages/login', get_user_info_json(req.cookies.user_idx));
-    // }catch(e){
-    //     // alert(e);
-    // }
   }
 });
-//ulogin
 
+///////https://easy.c4ei.net/rcv?rcv_email=his001_ccp@naver.com&rcv_adr=0x21725F3b26F74C8E451d851e040e717Fbcf19E5b&rcv_amt=0&tt=1636349389877
+router.get('/rcv', function(req, res, next) {
+  if (req.cookies.user_idx == "" || req.cookies.user_idx === undefined) {
+    res.sendFile(STATIC_PATH + '/ulogin.html')
+    return;
+  }
+  else {
+    /////////////////////////
+    // npm i sync-mysql
+    var db_config = require(__dirname + '/database.js');// 2020-09-13
+    var sync_mysql = require('sync-mysql'); //2020-01-28
+    let sync_connection = new sync_mysql(db_config.constr());
+
+    let user_email = req.cookies.user_email;
+    let result = sync_connection.query("SELECT id, c4ei_addr, c4ei_balance FROM user a WHERE a.email='" + user_email + "'");
+    let user_id = result[0].id;
+    let c4ei_addr = result[0].c4ei_addr;
+    let c4ei_balance = result[0].c4ei_balance;
+    if (c4ei_addr!="" && user_id > 0){
+      var wallet_balance = web3.eth.getBalance(c4ei_addr, function(error, result) {
+        // console.log("wallet_balance : "+ web3.utils.fromWei(result, "ether")); //0x21725F3b26F74C8E451d851e040e717Fbcf19E5b
+        wallet_balance = web3.utils.fromWei(result, "ether");
+        if (wallet_balance != c4ei_balance){
+          let result = sync_connection.query("update user set c4ei_balance='"+wallet_balance+"' WHERE id='" + user_id + "'");
+          console.log("wallet_balance :"+wallet_balance);
+          c4ei_balance = wallet_balance;
+        }
+      });
+    }
+    /////////////////////////
+    var rcv_email = req.query.rcv_email;
+    var rcv_adr = req.query.rcv_adr;
+    var rcv_amt = req.query.rcv_amt;
+    console.log("rcv_email :"+rcv_email + '/ rcv_adr : ' + rcv_adr);
+    res.render('rcv', { title: 'easyRcv', c4ei_addr : c4ei_addr, c4ei_balance : c4ei_balance, email: user_email, 
+      rcv_email :rcv_email,rcv_adr:rcv_adr,rcv_amt:rcv_amt});
+  }
+});
 
 router.get('/', auth(), awaitHandlerFactory(userController.getAllUsers)); // localhost:3000/api/v1/users
 router.get('/id/:id', auth(), awaitHandlerFactory(userController.getUserById)); // localhost:3000/api/v1/users/id/1
